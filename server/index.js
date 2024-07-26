c6846f097f1b1a5ca2d74fbff79c1fcb0939e5cc
beyondthefold777 <beyondthefold1989@gmail.com>
2024-07-26 15:17:37 -0400

    setting mongodb server with auth so our instance is secured

---
+++ server/index.js
@@ -0,0 +1,31 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables
+
+const app = express();
+const port = 3001;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Use environment variables for connection string
+const { MONGO_USER, MONGO_PASSWORD, MONGO_DB, MONGO_HOST, MONGO_PORT } = process.env;
+
+// Constructed the connection string for MongoDB with authentication
+const connectionString = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  authMechanism: 'SCRAM-SHA-256' 
+})
+  .then(() => {
+    console.log('Connected successfully to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server is listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Mongo connection error: ', err.message);
+  });
