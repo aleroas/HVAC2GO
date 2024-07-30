bedaf933c8a9822c20bd1d5801dbc600f95dc2ed
beyondthefold777 <beyondthefold1989@gmail.com>
2024-07-30 14:55:47 -0400

    added render domain to the allow domains in the cors set up

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Allow requests from our React app's domain
 app.use(cors({ 
-  origin: 'http://localhost:5173' 
+  origin: ['http://localhost:5173', 'https://hvac2go.onrender.com'] 
 }));
 
 // Use environment variables for connection string
