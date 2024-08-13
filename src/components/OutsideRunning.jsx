b7698be9887af80c9eec6f574391fa999809f90b
Juan Parra <parrajuan1025@gmail.com>
2024-08-13 12:18:47 -0400

    changing start command server back

--- src/components/OutsideRunning.jsx
+++ src/components/OutsideRunning.jsx
@@ -22,6 +22,7 @@ function OutsideRunning(){
             <li>
                 Check inside blower motot for operation status (if you cant hear it running. Follow video for inspection)
             </li>
+            <h2>!!WARNING!!  turn off power at braker panel before working on unit</h2>
             <div><iframe width="560" height="315" src="https://www.youtube.com/embed/glFsL73YmNU?si=lg4clAEQ1bmct3Ea" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
             <button className="px-4 py-2 text-lg font-medium text-white bg-teal-500 rounded hover:bg-teal-700" onClick={()=> navigate('/form')}> HELP ME</button>
             <button className="px-4 py-2 text-lg font-medium text-white bg-teal-500 rounded hover:bg-teal-700" onClick={()=> navigate('/air-conditioning/diag')}> Start Over</button>
