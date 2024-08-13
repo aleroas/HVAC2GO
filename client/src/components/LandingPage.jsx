e1a5a9ecb9c6c64c838af5a85dbe81a2f9858a90
beyondthefold777 <beyondthefold1989@gmail.com>
2024-08-13 10:13:19 -0400

    readd link as register and login buttons should go in top right corner, added background image, removed elements on the page so we can merge our code more easily

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,62 +1,17 @@
-import { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import { useNavigate } from 'react-router-dom';
-import './LandingPage.css'; // Import the new CSS file
+import { useEffect } from 'react';
 import Wave from './Wave'; // Ensure you import the Wave component
 
 const LandingPage = () => {
-  const navigate = useNavigate();
-  const [isTextVisible, setTextVisible] = useState(false);
-  const [isButtonsVisible, setButtonsVisible] = useState(false);
-
   useEffect(() => {
-    setTimeout(() => setTextVisible(true), 500);
-    setTimeout(() => setButtonsVisible(true), 1500);
+    // Any necessary side effects can go here
   }, []);
 
   return (
     <div className="landing-page">
-      <Wave /> {/* Add the Wave component here */}
       <div className="logo-container">
         <img src="/images/hvaclogo2.png" alt="Logo" className="logo" />
       </div>
-
-      <motion.div
-        initial={{ x: -1000 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 1 }}
-        className="text-center bg-white p-8 rounded-lg shadow-lg"
-      >
-     
-
-        {isButtonsVisible && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1 }}
-            className="button-container"
-          >
-            <button
-              onClick={() => navigate('/heating')}
-              className="option-button"
-            >
-              Heating
-            </button>
-            <button
-              onClick={() => navigate('/ventilation')}
-              className="option-button"
-            >
-              Ventilation
-            </button>
-            <button
-              onClick={() => navigate('/air-conditioning')}
-              className="option-button"
-            >
-              Air Conditioning
-            </button>
-          </motion.div>
-        )}
-      </motion.div>
+      <Wave />
     </div>
   );
 };
